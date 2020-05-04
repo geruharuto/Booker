fecce84bcd985b3254846db0c99760022c285dcd
geruharuto <geruharuto2121@yahoo.co.jp>
2020-05-04 23:04:16 +0900

    [update]5/4/23:00

---
+++ app/controllers/tops_controller.rb
@@ -0,0 +1,7 @@
+class TopsController < ApplicationController
+	def top
+	end
+	def index
+		redirect_to "/index"
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+	get "top" => "tops#top"
+	get "go" => "books#show"
+  resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
