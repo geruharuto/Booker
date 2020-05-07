c2bc1f70412cde95004a4e91a729cf9e9aa93171
geruharuto <geruharuto2121@yahoo.co.jp>
2020-05-07 21:43:09 +0900

    コントローラをbooksに変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,5 @@
-
-class BlogsController < ApplicationController
-  def top
-  end
-
-  def index
+class BooksController < ApplicationController
+	def index
   	@books = Book.all
     @book = Book.new
   end
@@ -11,26 +7,26 @@ class BlogsController < ApplicationController
   def create
   	book = Book.new(book_params)
   	book.save
-  	redirect_to blog_path(book)
+  	redirect_to book_path(book)
   end
 
   def show
-  	@blog = Blog.find(params[:id])
+  	@book = Book.find(params[:id])
   end
 
   def edit
-  	@book = Blog.find(params[:id])
+  	@book = Book.find(params[:id])
   end
   def update
   	book = Book.find(params[:id])
   	book.update(book_params)
-  	redirect_to show_path(book.id)
+  	redirect_to book_path(book.id)
   end
 
   def destroy
   		book = Book.find(params[:id])
   		book.destroy
-  		redirect_to index_path
+  		redirect_to books_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
+
+
 Rails.application.routes.draw do
-    resources :blogs
-    get "top" => "tops#top"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get "/" => "books#top"
+    resources :books
 end
